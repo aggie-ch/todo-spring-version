eeaeaf9e7e89d97c69a65c8b756fba0c56df64d1
aggie-ch <aggie-ch@github.com>
2020-04-03 17:31:25 +0200

    Added spring project

---
+++ src/main/java/io/github/aggie/todo/Todo.java
@@ -0,0 +1,53 @@
+package io.github.aggie.todo;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    private Integer id;
+    private String text;
+    private boolean done;
+
+    @SuppressWarnings("unused")
+    public Todo() {
+    }
+
+    public Todo(Integer id, String text, Boolean done) {
+        this.id = id;
+        this.text = text;
+        this.done = done;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+}
---
+++ src/main/resources/db/migration/V4__Add_todos.sql
@@ -0,0 +1,2 @@
+insert into todos (text, done) values ('backend', 0);
+insert into todos (text, done) values ('project', 1);
